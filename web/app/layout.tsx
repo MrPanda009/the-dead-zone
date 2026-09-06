15f5a46ba77fdda8c850599f8739885d89cee383
MrPanda009 <shrey33005@gmail.com>
2026-09-06 16:25:11 +0530

    fix: update Material Symbols configuration and preload font resources in layout

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -36,6 +36,15 @@ export default function RootLayout({
       suppressHydrationWarning
       className={`${plusJakarta.variable} ${spaceGrotesk.variable} ${jetbrainsMono.variable} dark h-full antialiased`}
     >
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
+        />
+      </head>
       <body
         suppressHydrationWarning
         className="min-h-full flex flex-col bg-bg-base text-text-primary font-sans overflow-hidden select-none selection:bg-citron/30 selection:text-citron"
